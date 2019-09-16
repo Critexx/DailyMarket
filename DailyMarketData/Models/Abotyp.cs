1fb8447a6f3d58da39c89b1d600cce8537d743d6
Pascal Schaffner <pascal.schaffner@hotmail.com>
2019-09-16 20:43:36 +0200

    Validierung implementiert (zwar auf englisch, aber im Moment egal)

--- DailyMarketData/Models/Abotyp.cs
+++ DailyMarketData/Models/Abotyp.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 
 namespace DailyMarketData.Models
@@ -6,13 +7,19 @@ namespace DailyMarketData.Models
     public partial class Abotyp
     {
         public int Id { get; set; }
+        [Required]
         public string Bezeichnung { get; set; }
         public string Beschreibung { get; set; }
+        [Required]
         public int Mietdauer { get; set; }
+        [Required]
         public int AnzahlBelegungNoetig { get; set; }
+        [Required]
         public DateTime GueltigVon { get; set; }
+        [Required]
         public DateTime? GueltigBis { get; set; }
-        public decimal RabattInProzent { get; set; }
+        [Required]
+        public decimal RabattInProzent { get; set; } = 0;
         public DateTime CreatedAt { get; set; }
         public string CreatedBy { get; set; }
         public DateTime? UpdatedAt { get; set; }
